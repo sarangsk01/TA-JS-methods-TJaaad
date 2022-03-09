7cc0db5e830a72264b69dec43cc57d16cef9ad50
Sarang Kulkarni <kulkarnisarang01@gmail.com>
2022-03-09 17:44:32 +0530

    Assignment:Array Methods

--- block-BJaadf/code/index.js
+++ block-BJaadf/code/index.js
@@ -1,21 +1,23 @@
 let fruits = ['Banana', 'Orange', 'Apple', 'Mango'];
 
 // Add a new element ('Papaya') into fruits array at end
-
+fruits.push('Papaya');
 // Remove the last element from fruits array
-
+fruits.pop();
 // Log the current length of fruits array
-
+console.log(fruits.length);
 // Remove the first element from fruits and log the value of fruits (use delete arr[0])
-
+delete fruits[0];
 // Log the element on index 0 and 1
-
+console.log(fruits[0],fruits[1]);
 // If the element at 0 index is undefined and index 1 is 'Banana' shift all the values to one lower index (use shift method)
 
 // Add a new element to the start ('Guava') and shift the index of all other to one higher value
-
+fruits.unshift('Guava');
+fruits.shift();
 // Add a new element to the start ('Dragon Fruit') and shift the index of all other to one higher value
-
+fruits.unshift('Dragon Fruit');
+fruits.shift();
 // Log true or false based on the value at index 0 and 1 is 'Dragon Fruit' and 'Guava' or not
 
 // Update the value of index 1 to `Pears`
